87e0eded10db25e3341efce2e56396c5e09df9eb
Hajarbany <97689037+Hajarbany@users.noreply.github.com>
2022-02-08 15:20:57 -0600

    Update bult-in-min-max.py

--- bult-in-min-max.py
+++ bult-in-min-max.py
@@ -1,1 +1,42 @@
+def maximum(value1, value2, value3):
+    """Return the maximum of three
+values."""
+    max_value = value1
+    if value2 > max_value:
+        max_value = value2
+    if value3 > max_value:
+        max_value = value3
+        
+    return max_value
+
+print('enter 3 numbers or strings to find the maximum')
+value1 = input('enter value 1: ')
+value2 = input('enter value 2: ')
+value3 = input('enter value 3: ')
+
+print('the maximum of your numbers is:', maximum(value1, value2, value3))
+
+maximum(12,27,55)
+
+           # Hajar Banyalmarjeh
+
+def minimum(value1, value2, value3):
+    """Return the minimum of three
+values."""
+    min_value = value1
+    if value2 < min_value:
+        min_value = value2
+    if value3 < min_value:
+        min_value = value3
+        
+    return min_value
+
+print('enter 3 numbers or strings to find the minimum')
+value1 = input('enter value 1: ')
+value2 = input('enter value 2: ')
+value3 = input('enter value 3: ')
+
+print('the minimum of your numbers is:', minimum(value1, value2, value3))
+
+minimum(12,27,55)
 
